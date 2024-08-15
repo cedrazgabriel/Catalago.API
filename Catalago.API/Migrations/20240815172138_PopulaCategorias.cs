53eedae4dc71d7783c09326a7e61ecb5b4a1e0f7
Gabriel Cedraz <cedrazdev@gmail.com>
2024-08-15 14:25:56 -0300

    feat: cria seed de categorias

---
+++ Catalago.API/Migrations/20240815172138_PopulaCategorias.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Catalago.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO categories(name, image_url) VALUES ('Bebidas', 'bebidas.jpg')");
+            migrationBuilder.Sql("INSERT INTO categories(name, image_url) VALUES ('Lanches', 'lanches.jpg')");
+            migrationBuilder.Sql("INSERT INTO categories(name, image_url) VALUES ('Sobremesas', 'sobremesas.jpg')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM categories");
+        }
+    }
+}
